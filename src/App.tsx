836bd4c5216a69643553dd4330ec77e711d7d210
kevinsmasters <gmail@kevinmasters.com>
2022-11-27 13:51:55 -0500

    simple style props demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,12 @@
 import './App.css';
-import { ButtonE } from './components/ButtonE';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Input } from './components/Input';
-import { Oscar } from './components/Oscar';
-import { Person } from './components/Person';
-import { Status } from './components/Status';
-import { PersonList } from './PersonList';
+import { Container } from './components/Container';
 
 function App() {
-  const personName = {
-    first: 'Person',
-    last: 'McPersonface'
-  }
-
-  const nameList = [
-    {
-      first: 'Kevin',
-      last: 'Mathpapers'
-    },
-    {
-      first: 'Sean',
-      last: 'Mathpapers'
-    },
-    {
-      first: 'Eric',
-      last: 'Paranormas'
-    }
-  ]
+  
   
   return (
     <div className="App">
-      {/* 
-      <Person name={personName} />
-      <PersonList names={nameList} /> */}
-      {/* <Status status='success' />
-      <Heading>Placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Your Mom!</Heading>
-      </Oscar>
-      <Greet 
-        name='Meowdy!' 
-        messageCount={69} 
-        isLoggedIn={true}  
-      /> */}
-      <ButtonE handleClick={(event, id) => {
-        console.log('Button clicked', event, id)
-      }} />
-      <Input value='' handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1rem'}} />
     </div>
   );
 }
