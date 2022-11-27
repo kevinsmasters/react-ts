ea21e4061872bedbc4aa475552ec2ec070a0cb46
kevinsmasters <gmail@kevinmasters.com>
2022-11-27 13:41:07 -0500

    handleChange as declared function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { ButtonE } from './components/ButtonE';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { Status } from './components/Status';
@@ -46,6 +47,7 @@ function App() {
       <ButtonE handleClick={(event, id) => {
         console.log('Button clicked', event, id)
       }} />
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   );
 }
